ca96f11e9b88393935ee26ff77865a50a3f70591
najwa2222 <127549435+najwa2222@users.noreply.github.com>
2024-06-10 00:12:47 +0200

    Update jest.config.js

--- views/jest.config.js
+++ views/jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
-  testPathIgnorePatterns: ["/node_modules/"],
+  testMatch: ['<rootDir>/__tests__/**/*.test.js'], // Chercher les fichiers de test avec l'extension .test.js dans le répertoire __tests__
+  testPathIgnorePatterns: ['<rootDir>/node_modules/'], // Ignorer les fichiers dans le répertoire node_modules
 };
+
